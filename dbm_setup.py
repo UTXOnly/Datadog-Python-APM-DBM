9b26e9d18a6311e00c21576517a78fe773164f74
Brian Hartford <brian.hartford@datadoghq.com>
2023-08-30 23:06:35 -0400

    Update dbm_setup.py
    update exception handling

--- dbm_setup.py
+++ dbm_setup.py
@@ -1,21 +1,14 @@
 # pylint: disable=W0621
 import os
 import time
-from dotenv import load_dotenv
 import psycopg2
 
-load_dotenv()
-
-GREEN = "\033[0;32m"
-RED = "\033[0;31m"
-RESET = "\033[0m"
-
 connection_params = {
-    'host': os.getenv('DB_HOST'),
-    'port': os.getenv('DB_PORT'),
-    'dbname': os.getenv('DB_NAME'),
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD')
+    'host': "localhost",
+    'port': '5432',
+    'dbname': 'nostr',
+    'user': 'nostr',
+    'password': 'nostr'
 }
 
 
@@ -35,43 +28,52 @@ def create_datadog_user_and_schema(conn_obj, db):
     if schema_exists:
         print(f"{RED}datadog schema already exists in db{RESET}")
 
-    else:
-
+    try:
         with conn_obj.cursor() as cur:
-            cur.execute("CREATE SCHEMA datadog; GRANT USAGE ON SCHEMA datadog TO datadog; GRANT USAGE ON SCHEMA public TO datadog; GRANT pg_monitor TO datadog; CREATE EXTENSION IF NOT EXISTS pg_stat_statements;")
+            cur.execute("CREATE EXTENSION IF NOT EXISTS pg_stat_statements;")
+    except Exception as e:
+        print(f"An error occurred while executing CREATE EXTENSION: {e}")
+    
+    try:
+        with conn_obj.cursor() as cur:
+            cur.execute("CREATE SCHEMA datadog; GRANT USAGE ON SCHEMA datadog TO datadog; GRANT USAGE ON SCHEMA public TO datadog; GRANT pg_monitor TO datadog;")
             print(f"{GREEN}datadog schema created and permissions granted in {db} database{RESET}")
             conn.commit()
-
+    except Exception as e:
+        print(f"An error occurred while creating datadog schema and granting permissions: {e}")
 
 def explain_statement(conn_obj):
-    with conn_obj.cursor() as cur:
-        cur.execute("""
-        CREATE OR REPLACE FUNCTION datadog.explain_statement(
-            l_query TEXT,
-            OUT explain JSON
-        )
-        RETURNS SETOF JSON AS
-        $$
-        DECLARE
-            curs REFCURSOR;
-            plan JSON;
-        BEGIN
-            OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
-            FETCH curs INTO plan;
-            CLOSE curs;
-            RETURN QUERY SELECT plan;
-        END;
-        $$
-        LANGUAGE 'plpgsql'
-        RETURNS NULL ON NULL INPUT
-        SECURITY DEFINER;
-        """)
-        conn_obj.commit()
-        time.sleep(2)
-    print(f"{GREEN}Explain plans statement completed{RESET}")
+    try:
+        with conn_obj.cursor() as cur:
+            cur.execute("""
+            CREATE OR REPLACE FUNCTION datadog.explain_statement(
+                l_query TEXT,
+                OUT explain JSON
+            )
+            RETURNS SETOF JSON AS
+            $$
+            DECLARE
+                curs REFCURSOR;
+                plan JSON;
+            BEGIN
+                OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
+                FETCH curs INTO plan;
+                CLOSE curs;
+                RETURN QUERY SELECT plan;
+            END;
+            $$
+            LANGUAGE 'plpgsql'
+            RETURNS NULL ON NULL INPUT
+            SECURITY DEFINER;
+            """)
+            conn_obj.commit()
+            time.sleep(2)
+        print(f"{GREEN}Explain plans statement completed{RESET}")
+    except Exception as e:
+        print(f"An error occurred while creating explain statement: {e}")
 
 
-def check_postgres_stats(connection_params, db):
+def check_postgres_stats(conn_obj, db):
     try:
         conn_obj = psycopg2.connect(**connection_params)
         with conn_obj.cursor() as cur:
@@ -98,16 +100,20 @@ def list_databases(conn_obj):
         databases = [row[0] for row in cur.fetchall() if not row[0].startswith('template')]
     return databases
 
+try:
+    conn = psycopg2.connect(**connection_params)
+    databases_list = list_databases(conn)
+except psycopg2.Error as e:
+    print("An error occurred while connecting to the database:", e)
 
-conn = psycopg2.connect(**connection_params)
-databases_list = list_databases(conn)
 
 # Iterate through the list of database names, run checks, and create schemas
 for db_name in databases_list:
     print(f"{GREEN}Discovered database: {RESET}{db_name} \nCreating schema and checking permissions + stats")
     connection_params['dbname'] = db_name
     conn = psycopg2.connect(**connection_params)
-    create_datadog_user_and_schema(conn, connection_params['dbname'])
-    check_postgres_stats(connection_params, db_name)
+    create_datadog_user_and_schema(conn_obj=conn, db=connection_params['dbname'])
+    explain_statement(conn_obj=conn)
+    check_postgres_stats(conn_obj=conn, db=db_name)
 
 print("Setup complete!")
